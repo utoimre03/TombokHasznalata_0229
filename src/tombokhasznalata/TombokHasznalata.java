9282c4d856c528d0a149f71939481fb012d346c7
Ütő Imre (SZF_2023_2025) <uto.imre@szamalk-szalezi.hu>
2024-02-29 15:46:42 +0100

    orai munka

---
+++ src/tombokhasznalata/TombokHasznalata.java
@@ -0,0 +1,20 @@
+
+package tombokhasznalata;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class TombokHasznalata {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        System.out.println("Kérem, adja meg, hányszor szeretne dobni a kockával! ");
+        int dobasokSzama = scanner.nextInt();
+        
+        
+        }
+        
+    }
+}
